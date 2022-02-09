a23b5676591700f456fecd31f2ec01bd3bfe5487
Toraudonn <toraudonn@gmail.com>
2022-02-09 13:15:14 +0000

    fix

--- sseg/datasets/__init__.py
+++ sseg/datasets/__init__.py
@@ -3,7 +3,7 @@ Copyright (C) 2019 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
 
-from datasets import cityscapes
+from sseg.datasets import cityscapes
 import torchvision.transforms as standard_transforms
 import torchvision.utils as vutils
 import transforms.joint_transforms as joint_transforms
